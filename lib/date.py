0ceb043c7cf2061b23601b3a12f537d7d93d68ab
VarnitS2 <varnits02@gmail.com>
2021-02-28 17:16:05 -0600

    Override __str__

--- lib/date.py
+++ lib/date.py
@@ -3,3 +3,6 @@ class Date:
         self.month = date.split('-')[0]
         self.day = date.split('-')[1]
         self.year = date.split('-')[2]
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.month, self.day, self.year)
