4f6cc1adc178e492860b30bbfb88d748b07cefb9
VarnitS2 <varnits02@gmail.com>
2021-03-02 15:45:21 -0600

    Implemented compare

--- lib/date.py
+++ lib/date.py
@@ -1,8 +1,24 @@
 class Date:
     def __init__(self, date):
-        self.month = date.split('-')[0]
-        self.day = date.split('-')[1]
-        self.year = date.split('-')[2]
+        self.month = int(date.split('-')[0])
+        self.day = int(date.split('-')[1])
+        self.year = int(date.split('-')[2])
 
     def __str__(self):
         return "{}-{}-{}".format(self.month, self.day, self.year)
+
+    def compare(self, other):
+        if other.year > self.year:
+            return -1
+        elif other.year < self.year:
+            return 1
+        elif other.month > self.month:
+            return -1
+        elif other.month < self.month:
+            return 1
+        elif other.day > self.day:
+            return -1
+        elif other.day < self.day:
+            return 1
+        else:
+            return 0
