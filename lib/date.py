b7bc9ab97795f07a9fcfb98d973472d1ee5dfd8b
VarnitS2 <varnits02@gmail.com>
2021-03-03 00:53:38 -0600

    Changed date format

--- lib/date.py
+++ lib/date.py
@@ -1,11 +1,11 @@
 class Date:
     def __init__(self, date):
-        self.month = int(date.split('-')[0])
-        self.day = int(date.split('-')[1])
-        self.year = int(date.split('-')[2])
+        self.month = int(date.split('/')[0])
+        self.day = int(date.split('/')[1])
+        self.year = int(date.split('/')[2])
 
     def __str__(self):
-        return "{}-{}-{}".format(self.month, self.day, self.year)
+        return "{}/{}/{}".format(self.month, self.day, self.year)
 
     def compare(self, other):
         if other.year > self.year:
